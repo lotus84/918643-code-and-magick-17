721afc3b63aa9c6474cd7bcbfa221407fda9aedd
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-06-13 16:14:47 +0400

    Написала функцию создания DOM-элемента на основе JS-объекта

--- js/setup.js
+++ js/setup.js
@@ -45,6 +45,20 @@ var getWizards = function (arrName, arrSurname, arrCoatColors, arrEyesColors) {
   return wizards;
 };
 
-getWizards(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS);
+var wizardsList = getWizards(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS);
 
+document.querySelector('.setup-similar').classList.remove('hidden');
 
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
