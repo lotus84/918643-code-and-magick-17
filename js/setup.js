02cb7569722e624911391787c39fde2ae061f3d8
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-06-17 11:40:58 +0400

    Упростила код (убрала лишние функции) и заменила магическое число на константу

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,7 @@ var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
 
 var getRandomNumber = function (arr) {
   var randomNumber = Math.floor(Math.random() * 10);
@@ -18,34 +19,23 @@ var getRandomNumber = function (arr) {
   return randomNumber;
 };
 
-var setWizardsName = function (arrName, arrSurname) {
-  var wizardsNames = [];
-  for (var i = 0; i < 4; i++) {
-    var name = arrName[getRandomNumber(arrName)];
-    var surname = arrSurname[getRandomNumber(arrSurname)];
-    wizardsNames[i] = name + ' ' + surname;
-  }
-  return wizardsNames;
+var getWizardsObject = function () {
+  return {
+    name: NAMES[getRandomNumber(NAMES)] + ' ' + SURNAMES[getRandomNumber(SURNAMES)],
+    coatColor: COAT_COLORS[getRandomNumber(COAT_COLORS)],
+    eyesColor: EYES_COLORS[getRandomNumber(EYES_COLORS)]
+  };
 };
 
-var getWizards = function (arrName, arrSurname, arrCoatColors, arrEyesColors) {
+var getWizards = function () {
   var wizards = [];
-
-  for (var i = 0; i < 4; i++) {
-    var wizardObject = {};
-    var name = setWizardsName(arrName, arrSurname)[i];
-    var coatColor = arrCoatColors[getRandomNumber(arrCoatColors)];
-    var eyesColor = arrEyesColors[getRandomNumber(arrEyesColors)];
-    wizardObject.name = name;
-    wizardObject.coatColor = coatColor;
-    wizardObject.eyesColor = eyesColor;
-    wizards.push(wizardObject);
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    wizards.push(getWizardsObject());
   }
-
   return wizards;
 };
 
-var wizardsList = getWizards(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS);
+var wizardsList = getWizards();
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
