4c8af36994eb8fe9179ce739d444981833b78dbe
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-07-07 17:15:16 +0400

    Сделала модуль setup изолированным

--- js/setup.js
+++ js/setup.js
@@ -1,64 +1,63 @@
 'use strict';
 
+(function () {
+  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var WIZARDS_COUNT = 4;
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARDS_COUNT = 4;
+  var getRandomNumber = function (arr) {
+    var randomNumber = Math.floor(Math.random() * 10);
 
-var getRandomNumber = function (arr) {
-  var randomNumber = Math.floor(Math.random() * 10);
+    if (randomNumber > arr.length - 1) {
+      randomNumber = arr.length - 1;
+    }
 
-  if (randomNumber > arr.length - 1) {
-    randomNumber = arr.length - 1;
-  }
-
-  return randomNumber;
-};
-
-var getWizardsObject = function () {
-  return {
-    name: NAMES[getRandomNumber(NAMES)] + ' ' + SURNAMES[getRandomNumber(SURNAMES)],
-    coatColor: COAT_COLORS[getRandomNumber(COAT_COLORS)],
-    eyesColor: EYES_COLORS[getRandomNumber(EYES_COLORS)]
+    return randomNumber;
   };
-};
 
-var getWizards = function () {
-  var wizards = [];
-  for (var i = 0; i < WIZARDS_COUNT; i++) {
-    wizards.push(getWizardsObject());
-  }
-  return wizards;
-};
+  var getWizardsObject = function () {
+    return {
+      name: NAMES[getRandomNumber(NAMES)] + ' ' + SURNAMES[getRandomNumber(SURNAMES)],
+      coatColor: window.util.COAT_COLORS[getRandomNumber(window.util.COAT_COLORS)],
+      eyesColor: window.util.EYES_COLORS[getRandomNumber(window.util.EYES_COLORS)]
+    };
+  };
 
-var wizardsList = getWizards();
+  var getWizards = function () {
+    var wizards = [];
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
+      wizards.push(getWizardsObject());
+    }
+    return wizards;
+  };
 
-document.querySelector('.setup-similar').classList.remove('hidden');
+  var wizardsList = getWizards();
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-.content
-.querySelector('.setup-similar-item');
+  document.querySelector('.setup-similar').classList.remove('hidden');
 
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
 
-  return wizardElement;
-};
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-var createFragment = function (wizards) {
-  var fragment = document.createDocumentFragment();
+    return wizardElement;
+  };
 
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
+  var createFragment = function (wizards) {
+    var fragment = document.createDocumentFragment();
 
-  return similarListElement.appendChild(fragment);
-};
+    for (var i = 0; i < wizards.length; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
 
-createFragment(wizardsList);
+    return similarListElement.appendChild(fragment);
+  };
+
+  createFragment(wizardsList);
+})();
