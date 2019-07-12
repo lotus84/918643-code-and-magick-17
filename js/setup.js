4c11bda465bdebee9563417b83f60ded824dc2c9
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-07-12 20:11:58 +0400

    Удалила моковые данные

--- js/setup.js
+++ js/setup.js
@@ -1,37 +1,7 @@
 'use strict';
 
 (function () {
-  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var WIZARDS_COUNT = 4;
-
-  var getRandomNumber = function (arr) {
-    var randomNumber = Math.floor(Math.random() * 10);
-
-    if (randomNumber > arr.length - 1) {
-      randomNumber = arr.length - 1;
-    }
-
-    return randomNumber;
-  };
-
-  var getWizardsObject = function () {
-    return {
-      name: NAMES[getRandomNumber(NAMES)] + ' ' + SURNAMES[getRandomNumber(SURNAMES)],
-      coatColor: window.util.COAT_COLORS[getRandomNumber(window.util.COAT_COLORS)],
-      eyesColor: window.util.EYES_COLORS[getRandomNumber(window.util.EYES_COLORS)]
-    };
-  };
-
-  var getWizards = function () {
-    var wizards = [];
-    for (var i = 0; i < WIZARDS_COUNT; i++) {
-      wizards.push(getWizardsObject());
-    }
-    return wizards;
-  };
-
-  var wizardsList = getWizards();
 
   document.querySelector('.setup-similar').classList.remove('hidden');
 
@@ -43,21 +13,21 @@
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
 
-  var createFragment = function (wizards) {
+  var onLoadWizards = function (wizards) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < wizards.length; i++) {
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
       fragment.appendChild(renderWizard(wizards[i]));
     }
 
     return similarListElement.appendChild(fragment);
   };
 
-  createFragment(wizardsList);
+  window.backend.load(onLoadWizards, window.util.onErrorLoad);
 })();
