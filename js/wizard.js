9d2523e2f2aaaff6204988786cbbd54ba0a17917
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-07-12 20:15:49 +0400

    Данные о цвете плаща и глаз мага перенесла сюда

--- js/wizard.js
+++ js/wizard.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var wizardItem = window.util.setup.querySelector('.setup-wizard');
   var wizardCoat = wizardItem.querySelector('.wizard-coat');
@@ -21,21 +23,21 @@
   };
 
   var changeCoatColor = function (index) {
-    wizardCoat.style.fill = window.util.COAT_COLORS[index];
-    wizardCoatInput.setAttribute('value', window.util.COAT_COLORS[index]);
+    wizardCoat.style.fill = COAT_COLORS[index];
+    wizardCoatInput.setAttribute('value', COAT_COLORS[index]);
   };
 
   wizardCoat.addEventListener('click', function () {
-    changeCoatColor(getArrayIndex(window.util.COAT_COLORS));
+    changeCoatColor(getArrayIndex(COAT_COLORS));
   });
 
   var changeEyesColor = function (index) {
-    wizardEyes.style.fill = window.util.EYES_COLORS[index];
-    wizardEyesInput.setAttribute('value', window.util.EYES_COLORS[index]);
+    wizardEyes.style.fill = EYES_COLORS[index];
+    wizardEyesInput.setAttribute('value', EYES_COLORS[index]);
   };
 
   wizardEyes.addEventListener('click', function () {
-    changeEyesColor(getArrayIndex(window.util.EYES_COLORS));
+    changeEyesColor(getArrayIndex(EYES_COLORS));
   });
 
   var changeFireballColor = function (index) {
