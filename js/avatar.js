a8921627752e145148517d67cd4e246a3e615cc2
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-07-24 13:52:47 +0400

    Создала и подключила модуль avatar, который будет отвечать за загрузку аватарки пользователя

---
+++ js/avatar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+
+  });
+})();
